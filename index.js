323324b69bcaa721564c45ec05e5d4317a7f22cd
nav2010 <39226429+nav2010@users.noreply.github.com>
2023-01-31 22:06:04 +0000

    modi

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-let currentDayE1=moment().format('LLL');
+let currentDayE1=moment().format('LLLL');
 $("#currentDay").text(currentDayE1);
 let containerE1=$(".container");
 let hourE1=$(".hour");
+
+
+
+
+
+    
+    
+    
