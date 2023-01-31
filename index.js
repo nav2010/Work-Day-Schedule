5c910bf7ea938dc48f30b21c48907fb915317292
nav2010 <39226429+nav2010@users.noreply.github.com>
2023-01-31 22:28:20 +0000

    modjji

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ let currentDayE1=moment().format('LLLL');
 $("#currentDay").text(currentDayE1);
 let containerE1=$(".container");
 let currentTimeE1=moment().hour();
+let startHour = 9;
+let endHour = 5;
+let timeBlockEls = $(".time-block");
+let containerEl = $(".container");
+
+let saveBtns = $(".saveBtn");
+let textInput = $("textarea");
 
 
 
