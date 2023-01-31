1fdf3b06f532a9fb33cbd8a465c80751017c7d4e
nav2010 <39226429+nav2010@users.noreply.github.com>
2023-01-31 23:16:52 +0000

    modjji

--- index.js
+++ index.js
@@ -2,13 +2,38 @@ let currentDayE1=moment().format('LLLL');
 $("#currentDay").text(currentDayE1);
 let containerE1=$(".container");
 let currentTimeE1=moment().hour();
-let startHour = 9;
-let endHour = 5;
-let timeBlockEls = $(".time-block");
-let containerEl = $(".container");
+var Hour = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+var saveBtn = document.querySelectorAll(".saveBtn");
+console.log(currentDayE1);
 
-let saveBtns = $(".saveBtn");
-let textInput = $("textarea");
+
+
+for (var i = 0; i < 9; i++) {
+  if (currentDayE1 === Hour[i]) {
+    console.log(i);
+    document.getElementById(i).classList.add("present");
+  }
+  if (currentDayE1 > Hour[i]) {
+    console.log(i);
+    document.getElementById(i).classList.add("past");
+  }
+  if (currentDayE1 < Hour[i]) {
+    console.log(i);
+    document.getElementById(i).classList.add("future");
+  }
+}
+
+  // accessing key name according to id + repop. data from local storage
+  if(localStorage.getItem(i)){
+      document.getElementById(i).value = localStorage.getItem(i);
+  }
+
+  saveBtn[i].addEventListener("click",function(){
+    var id = this.getAttribute("data-id");
+    var text = document.getElementById(id).value;
+     localStorage.setItem(id,text);
+    console.log(text);
+})
 
 
 
